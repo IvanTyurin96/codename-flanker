452d21f6e2d98f05c5820bd127a8e1d03397c2da
IvanTyurin <shtrinka@gmail.com>
2023-06-20 20:41:35 +0300

    Footer anchors.

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,16 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { ReactComponent as VkLogo } from "../svg/vk.svg";
 
 const Footer = () => {
   return (
     <footer className="mt-auto bg-dark">
       <div className="container column d-flex align-items-center justify-content-between">
         <div className="text-center pl-0 pt-3 pb-3 pr-3">© 2023 Ivan Tyurin</div>
-        <div className="text-center">
-          <a className="btn btn-outline-light btn-floating btn-sm m-1" href="https://discord.gg/codename-flanker-community-839196573228335185" role="button">
+        <div className="text-center d-flex">
+          <a className="footer-society me-2 d-flex align-items-center justify-content-center" href="https://vk.com/codenameflanker">
+            <VkLogo className="bi-vk" />
+          </a>
+          <a className="footer-society me-2" href="https://discord.gg/codename-flanker-community-839196573228335185" role="button">
             <i className="bi bi-discord"></i>
           </a>
-          <a className="btn btn-outline-light btn-floating btn-sm m-1" href="https://www.youtube.com/@codenameflanker1751" role="button">
+          <a className="footer-society" href="https://www.youtube.com/@codenameflanker1751" role="button">
             <i className="bi bi-youtube"></i>
           </a>
         </div>
