f357536a38a9c00a8a0596c0ec9727218318dd3f
IvanTyurin <shtrinka@gmail.com>
2023-11-14 20:24:56 +0300

    gallery

--- src/app/layouts/gallery.jsx
+++ src/app/layouts/gallery.jsx
@@ -16,32 +16,6 @@ const Gallery = () => {
   const [artists, setArtists] = useState([]);
   const [fetchArtistsError, setFetchArtistsError] = useState(null);
 
-  // const [currentArtistId, setCurrentArtistId] = useState(null)
-
-  //const sortedArtworks = useSelector((state) => state.sortedArtworks);
-  // const [sortedArtworks, setSortedArtworks] = useState([]);
-  // const [filteredArtworks, setFilteredArtworks] = useState(sortedArtworks);
-
-  //const [artistName, setArtistName] = useState("All");
-  // const updateArtistFilter = (artistId) => {
-  //   if (artistId === 0) {
-  //     setFilteredArtworks(sortedArtworks);
-  //     setArtistName("All");
-  //   } else {
-  //     setFilteredArtworks(sortedArtworks.filter((element) => element.artistId === artistId));
-  //     setArtistName(artists.find((artist) => artist.id === artistId).name);
-  //   }
-  // };
-
-  function getArtistId() {
-    const artistId = parseInt(searchParams.get("artist"));
-    if (artistId !== null && Number.isInteger(artistId)) {
-      return artistId;
-    } else {
-      return 0;
-    }
-  }
-
   function show() {
     return (
       <div className="pt-3 pb-3">
@@ -49,79 +23,6 @@ const Gallery = () => {
         <>{showArtworks()}</>
       </div>
     );
-  }
-
-  function showArtworks() {
-    if (fetchArtworksError === null) {
-      return renderArtworksData();
-    } else {
-      return renderArtworksError();
-    }
-  }
-
-  function renderArtworksData() {
-    if (artworks.length > 0) {
-      return showFilteredArtworks(getFilteredArtworks(getArtistId()));
-    } else {
-      return (
-        <div className="mt-2 d-flex flex-column align-items-center">
-          <LoadingSpinner />
-        </div>
-      );
-    }
-  }
-  function showFilteredArtworks(filteredArtworks) {
-    if (filteredArtworks.length > 0) {
-      return (
-        <div className={"mt-2" + (windowWidth >= screenCollapseWidth ? " gallery-grid" : " gallery-grid gallery-grid-mobile")}>
-          {filteredArtworks.map((artwork) => {
-            const artist = artists.find((element) => element.id === artwork.artistId);
-            return (
-              <div key={artwork.id} className="gallery-thumbnail-container" href="/test">
-                <img className="gallery-thumbnail" src={artwork.thumbnailBytes} alt="Artwork image" />
-                <div className="gallery-selected">
-                  <a className="gallery-thumbnail-anchor" href={"/gallery/" + artwork.id}></a>
-                  <div className="d-flex mb-3 mt-auto ps-3">
-                    <img className="artist-icon gallery-artist-icon" src={getArtistIcon(artist)}></img>
-                    <div className="ps-2 d-flex flex-column">
-                      <h5 className="m-0">{artwork.name}</h5>
-                      <h5 className="m-0 mt-auto">{getArtistName(artist)}</h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      );
-    } else {
-      return <div className="mt-2">No one artwork found for artist = {getArtistId()}</div>;
-    }
-  }
-  function getFilteredArtworks(artistId) {
-    if (artistId == 0) {
-      return artworks;
-    } else {
-      return artworks.filter((element) => element.artistId === artistId);
-    }
-  }
-  function getArtistIcon(artist) {
-    if (artist != null) {
-      return artist.iconBytes;
-    } else {
-      return require(`../icons/question_mark.png`);
-    }
-  }
-  function getArtistName(artist) {
-    if (artist != null) {
-      return artist.name;
-    } else {
-      return "Unknown artist";
-    }
-  }
-
-  function renderArtworksError() {
-    return <div className="error">{fetchArtworksError.toString()}</div>;
   }
 
   function showArtists() {
@@ -135,13 +36,32 @@ const Gallery = () => {
           aria-haspopup="true"
           aria-expanded="false"
         >
-          Artist:
+          Artist: {getArtistNameById(getArtistIdFromQuery())}
         </button>
         <div className="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton">
           {renderArtistsData(artists)}
         </div>
       </div>
     );
+  }
+
+  function getArtistNameById(artistId) {
+    if (artistId == 0) return "All";
+    const artist = artists.filter((element) => element.id == artistId)[0];
+    if (artist != null) {
+      return artist.name;
+    } else {
+      return "Unknown artist";
+    }
+  }
+
+  function getArtistIdFromQuery() {
+    const artistId = parseInt(searchParams.get("artist"));
+    if (artistId !== null && Number.isInteger(artistId)) {
+      return artistId;
+    } else {
+      return 0;
+    }
   }
 
   function renderArtistsData(artists) {
@@ -151,6 +71,7 @@ const Gallery = () => {
       return renderArtistsError();
     }
   }
+
   function renderArtistsList(artists) {
     if (artists.length > 0) {
       return (
@@ -180,6 +101,83 @@ const Gallery = () => {
 
   function renderArtistsError() {
     return <div className="error ms-2 me-2">{fetchArtistsError.toString()}</div>;
+  }
+
+  function showArtworks() {
+    if (fetchArtworksError === null) {
+      return renderArtworksData();
+    } else {
+      return renderArtworksError();
+    }
+  }
+
+  function renderArtworksData() {
+    if (artworks.length > 0) {
+      return showFilteredArtworks(getFilteredArtworks(getArtistIdFromQuery()));
+    } else {
+      return (
+        <div className="mt-2 d-flex flex-column align-items-center">
+          <LoadingSpinner />
+        </div>
+      );
+    }
+  }
+
+  function showFilteredArtworks(filteredArtworks) {
+    if (filteredArtworks.length > 0) {
+      return (
+        <div className={"mt-2" + (windowWidth >= screenCollapseWidth ? " gallery-grid" : " gallery-grid gallery-grid-mobile")}>
+          {filteredArtworks.map((artwork) => {
+            const artist = artists.find((element) => element.id === artwork.artistId);
+            return (
+              <div key={artwork.id} className="gallery-thumbnail-container" href="/test">
+                <img className="gallery-thumbnail" src={artwork.thumbnailBytes} alt="Artwork image" />
+                <div className="gallery-selected">
+                  <a className="gallery-thumbnail-anchor" href={"/gallery/" + artwork.id}></a>
+                  <div className="d-flex mb-3 mt-auto ps-3">
+                    <img className="artist-icon gallery-artist-icon" src={getArtistIcon(artist)}></img>
+                    <div className="ps-2 d-flex flex-column">
+                      <h5 className="m-0">{artwork.name}</h5>
+                      <h5 className="m-0 mt-auto">{getArtistName(artist)}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return <div className="mt-2">No one artwork found for artist = {getArtistIdFromQuery()}</div>;
+    }
+  }
+
+  function getFilteredArtworks(artistId) {
+    if (artistId == 0) {
+      return artworks;
+    } else {
+      return artworks.filter((element) => element.artistId === artistId);
+    }
+  }
+
+  function getArtistIcon(artist) {
+    if (artist != null) {
+      return artist.iconBytes;
+    } else {
+      return require(`../icons/question_mark.png`);
+    }
+  }
+
+  function getArtistName(artist) {
+    if (artist != null) {
+      return artist.name;
+    } else {
+      return "Unknown artist";
+    }
+  }
+
+  function renderArtworksError() {
+    return <div className="error">{fetchArtworksError.toString()}</div>;
   }
 
   useEffect(() => {
