3f0b772e6d77c47257e01b61183d8f1ce644972b
IvanTyurin96 <shtrinka@gmail.com>
2023-12-12 13:15:24 +0300

    Footer - links opening in new tab

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
       <div className="container column d-flex align-items-center justify-content-between">
         <div className="text-center pl-0 pt-3 pb-3 pr-3">© 2023 Ivan Tyurin</div>
         <div className="text-center d-flex">
-          <a className="footer-society me-2 d-flex align-items-center justify-content-center" href="https://vk.com/codenameflanker">
+          <a className="footer-society me-2 d-flex align-items-center justify-content-center" href="https://vk.com/codenameflanker" target="_blank">
             <VkLogo className="bi-vk" />
           </a>
-          <a className="footer-society me-2" href="https://discord.gg/codename-flanker-community-839196573228335185" role="button">
+          <a className="footer-society me-2" href="https://discord.gg/codename-flanker-community-839196573228335185" target="_blank" role="button">
             <i className="bi bi-discord"></i>
           </a>
-          <a className="footer-society" href="https://www.youtube.com/@codenameflanker1751" role="button">
+          <a className="footer-society" href="https://www.youtube.com/@codenameflanker1751" target="_blank" role="button">
             <i className="bi bi-youtube"></i>
           </a>
         </div>
