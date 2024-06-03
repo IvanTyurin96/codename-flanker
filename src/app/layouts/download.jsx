df526050ce8b97c33563a5a3f1ec94f866769dd0
IvanTyurin <shtrinka@gmail.com>
2024-06-03 20:58:47 +0300

    2.7.73

--- src/app/layouts/download.jsx
+++ src/app/layouts/download.jsx
@@ -3,62 +3,50 @@ import GoogleDriveIcon from "../icons/google-drive.png";
 const Download = () => {
   return (
     <div className="pt-3 pb-3">
-      <h3 className="mb-2">Su-30 Community Mod v2.7.72 Beta</h3>
+      <h3 className="mb-2">Su-30 Community Mod v2.7.73 Beta</h3>
       <em>Multiple links are for redundancy. All links below have the exact same file.</em>
       <p className="mt-2 mb-2">Links:</p>
       <ul className="mt-2 mb-2 download-list">
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1un6OX4ZhP04RW4LBvE3kPIhX2GzVdnZU/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1un6OX4ZhP04RW4LBvE3kPIhX2GzVdnZU/view?usp=sharing
+          <a href="https://drive.google.com/file/d/1kCbVs8Nz9L_X51iNBEt8qQDf0a1yw8c7/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1kCbVs8Nz9L_X51iNBEt8qQDf0a1yw8c7/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/127H6PNdv_EV521adAzPDUYRpwu5jvVqe/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/127H6PNdv_EV521adAzPDUYRpwu5jvVqe/view?usp=sharing
+          <a href="https://drive.google.com/file/d/1VYWlNIAmNbZwOs3NLnj-z_8z9aNyZ3T4/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1VYWlNIAmNbZwOs3NLnj-z_8z9aNyZ3T4/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/10yQ07NKgAiudxDy9ZS-GZ1CF6oDhqT1y/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/10yQ07NKgAiudxDy9ZS-GZ1CF6oDhqT1y/view?usp=sharing
+          <a href="https://drive.google.com/file/d/1Riq6wjZGpnlNcynTgYmY3oX3OZzBf6og/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1Riq6wjZGpnlNcynTgYmY3oX3OZzBf6og/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/12X6BXiloY1-CvLGmrCHTUVr4mVP3z1sT/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/12X6BXiloY1-CvLGmrCHTUVr4mVP3z1sT/view?usp=sharing
+          <a href="https://drive.google.com/file/d/1H9MzKiRnnJvzz6C1NWRuPlFfpTNoLMHF/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1H9MzKiRnnJvzz6C1NWRuPlFfpTNoLMHF/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1Tx5CLBsbejO0W2OP-Xqc2cBUdZqkH1NN/view?usp=drive_link" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1Tx5CLBsbejO0W2OP-Xqc2cBUdZqkH1NN/view?usp=drive_link
+          <a href="https://drive.google.com/file/d/13h3NT1BpVN5THxjsNWTwn7_-FtZZguhk/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/13h3NT1BpVN5THxjsNWTwn7_-FtZZguhk/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1IQVsXaep4HifUepEPnh_YqqniF2IbjTu/view?usp=drive_link" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1IQVsXaep4HifUepEPnh_YqqniF2IbjTu/view?usp=drive_link
+          <a href="https://drive.google.com/file/d/1uzIgme_IA51oTjFBdBkqCnovm3VTinkd/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1uzIgme_IA51oTjFBdBkqCnovm3VTinkd/view?usp=sharing
           </a>
         </li>
         <li>
           <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1iMqvwSs20THaWm2DJ2-MyaK9ZTIDxBN6/view?usp=drive_link" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1iMqvwSs20THaWm2DJ2-MyaK9ZTIDxBN6/view?usp=drive_link
-          </a>
-        </li>
-        <li>
-          <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1TgARXRqASA2Ls_MPLpc_zyP58qJTJoub/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1TgARXRqASA2Ls_MPLpc_zyP58qJTJoub/view?usp=sharing
-          </a>
-        </li>
-        <li>
-          <img className="download-icon" src={GoogleDriveIcon}></img>
-          <a href="https://drive.google.com/file/d/1hVAhnbb3zUmaUceoxjHk2VEG2FBY7so5/view?usp=sharing" target="_blank" className="link-primary text-break">
-            https://drive.google.com/file/d/1hVAhnbb3zUmaUceoxjHk2VEG2FBY7so5/view?usp=sharing
+          <a href="https://drive.google.com/file/d/1FXAVm7jj6H6gseKMk67NwKKnBCbBUFNr/view?usp=sharing" target="_blank" className="link-primary text-break">
+            https://drive.google.com/file/d/1FXAVm7jj6H6gseKMk67NwKKnBCbBUFNr/view?usp=sharing
           </a>
         </li>
       </ul>
